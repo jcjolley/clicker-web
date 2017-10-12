073a44869235b0dd43c001534f225baf5afb23b8
Joshua Jolley <jolleyboy@gmail.com>
2017-10-11 22:22:12 -0600

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from './websocket.service';
+import {Observable, Subject } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  public messages: Subject<any>
+  constructor(private websocketService: WebsocketService) {};
+
+  ngOnInit(): void {
+    this.messages = <Subject<any>>this.websocketService.connect('ws://localhost:3000')
+    .map((response: MessageEvent): any => {
+      console.log('response: ', JSON.stringify(response));
+      return response.data
+    })
+
+    this.messages.subscribe(msg => {
+      console.log("Response from websocket: " + msg)
+    })
+
+  }
+
+  sendMsg() {
+    this.messages.next({user: 'Jolley', copperOre: 10});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WebsocketService } from './websocket.service'
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    WebsocketService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
